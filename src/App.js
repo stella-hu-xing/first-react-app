87326cb7375931d366f9fe3587c3ef6d85dcf081
stella-hu <xinghu0164@gmail.com>
2022-05-08 00:10:18 +1000

    add date component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import './App.css';
 import {ExpenseItem} from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    {id: 1, title: 'Supermarket', amount: 23.1, date: new Date(2011, 2, 12)},
+    {id: 2, title: 'Car', amount: 472, date: new Date(2021, 6, 12)},
+    {id: 3, title: 'House', amount: 982.1, date: new Date(2022, 4, 4)},
+  ];
   return (
-    <div className="App">
-      <h2>Let's get started</h2>
-      <ExpenseItem />
+    <div>
+      <h2>Expense Items</h2>
+      {expenses.map((e) => {
+        return <ExpenseItem expense={e} key={e.id}></ExpenseItem>;
+      })}
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,3 +1,16 @@
-export function ExpenseItem() {
-  return <h2>Expense Item</h2>;
+import './ExpenseItem.css';
+import {ExpenseDate} from './ExpenseDate';
+
+export function ExpenseItem(props) {
+  const expense = props.expense;
+
+  return (
+    <div className="expense-item">
+      <ExpenseDate date={expense.date} />
+      <div className="expense-item__description">
+        <h2>{expense.title}</h2>
+        <div className="expense-item__price">${expense.amount}</div>
+      </div>
+    </div>
+  );
 }
